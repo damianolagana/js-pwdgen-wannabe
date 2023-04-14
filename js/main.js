e7a9ba28e591bae03032845c785363ff189c370a
damianolagana <damianolagana@gmail.com>
2023-04-14 11:35:26 +0200

    password

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 let nome = prompt("Inserisci il tuo nome")
 let cognome = prompt("Inserisci il tuo cognome")
 let colore = prompt("Inserisci il tuo colore preferito")
+
+let password = `${nome}-${cognome}-${colore}-2023`;
+
+document.getElementById("password").innerText = password;
